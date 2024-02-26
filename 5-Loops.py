c4c2d555519410221201ee1b0e0123017c0141b6
Touafic BOUAB <eclairetoilegentlemans@gmail.com>
2024-02-26 17:15:44 -0500

    Problem 5 solved

---
+++ 5-Loops.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 26 13:54:20 2024
+
+@author: touaf
+"""
+
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(n):
+        print(i ** 2)
