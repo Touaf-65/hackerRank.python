68bae1672dde1d9a217dae32b647220afe16ab46
Touafic BOUAB <eclairetoilegentlemans@gmail.com>
2024-02-26 16:36:36 -0500

    Problem 4 solved

---
+++ 4-Division.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 26 13:54:02 2024
+
+@author: touaf
+"""
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a // b)
+    print(a / b)
