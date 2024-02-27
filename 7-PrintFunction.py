db8dd6c14a150833078a00f21a169dcd3ae4f539
Touafic BOUAB <eclairetoilegentlemans@gmail.com>
2024-02-27 07:27:00 -0500

    Problem 7 solved

---
+++ 7-PrintFunction.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 26 15:30:55 2024
+
+@author: touaf
+"""
+
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(1,n+1):
+        print(i, end='')
